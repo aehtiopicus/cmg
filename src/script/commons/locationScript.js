58ef70c4eef7d8751c8bea234a43a7a129bb4380
jvega <jvega@belatrixsf.com>
2016-01-14 18:07:29 -0300

    adding menu

--- src/script/commons/locationScript.js
+++ src/script/commons/locationScript.js
@@ -2,10 +2,12 @@ var singlePage = (function(changeBody){
 	var sp ={
 	prevLocation : location.href,
 	changeBody : changeBody,
+	hashName : location.hash.substring(1),
 	locationChange : function(){
 		if(sp.prevLocation !== location.href){
 			sp.hashLocation = location.href;
-			  $("#"+sp.changeBody).load(location.hash.substring(1)+".html",function(page,simpleStatus,petition){
+			sp.hashName = location.hash.substring(1);
+			  $("#"+sp.changeBody).load(sp.hashName+".html",function(page,simpleStatus,petition){
 			  	if(typeof petition !== "undefined" && petition.status != 200){
 			  			$("#"+sp.changeBody).html("404 - No hay carton");
 			  	}
