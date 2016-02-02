33e300db02dc4c0900d03ff486eaae0c321a55b7
jvega <jvega@belatrixsf.com>
2016-02-02 18:16:30 -0300

    changes

--- src/script/contacto.js
+++ src/script/contacto.js
@@ -14,13 +14,69 @@
 					$("#"+_config.pageContainer).height(window.innerHeight*0.75);
 				},this));
 			},this));
-		
-		$("#"+_config.popUpId).on('hide.bs.modal', _.bind(function (a,b,c) {
-			$(window).off('resize.cmgModal');
-		},this));			
+
+			$("#"+_config.popUpId).on('hide.bs.modal', _.bind(function (a,b,c) {
+				$(window).off('resize.cmgModal');
+			},this));			
 			return publicAttribs;
 		};
+		
+		var _showSpinner = function(){
+			debugger;
+			var newOverlay = $("<div class='modal-backdrop in'></div>");
+			newOverlay.css("z-index","1050");
+			$("body").append(newOverlay);
+			$(".spinnerDiv").removeClass("hidden");
+		};
+
+		var _hideSpinner = function(){
+			$(".spinnerDiv").addClass("hidden");
+			$($(".modal-backdrop.in")[1]).remove();
+			
+
+		};
+		var _sendEmail = function(formId){
+			
+			_showSpinner();
+			var form = document.getElementById(formId);
+			var preventPost = false;
+			for(var i = 0; i < form.length-1 ; i++){
+				if(!form[i].checkValidity()){
+					$("#"+form[i].id).addClass("invalid");
+					preventPost = true;
+				}else{
+					$("#"+form[i].id).removeClass("invalid");
+				}
+			}
+			var postData = {nombre: form['nombre'].value,
+			telefono: form['telefono'].value,
+			email: form['email'].value,
+			mensaje: form['mensaje'].value,
+			toemail: form['toemail'].value};
+			if(!preventPost){
+				$.ajax({
+					type : "POST",
+					url : "https://script.google.com/macros/s/AKfycbwrDeg8sJbrSGeFfED5BKcy9livNvCz1Qy2gtA0v9qxuPh2Qw4/exec",				
+					data : postData,
+					success : function(data,textStatus,jqXHR){
+						$("#"+formId+" input:not(:hidden)").val("");
+						$("#"+formId+" textarea").val("");
+						_hideSpinner();
+						publicAttribs.closeModal();
+							
 						
+					},
+					error : function(data,textStatus,jqXHR){
+						_hideSpinner();
+						debugger;
+					}
+
+				});
+			}else{
+				_hideSpinner();
+			}
+
+		}						
 		var publicAttribs =  {
 			openModal : function(){
 				$("#"+_config.pageContainer).height(window.innerHeight*.75); 
@@ -29,6 +85,9 @@
 			closeModal : function(){
 
 				$("#"+_config.popUpId).modal("hide");	
+			},
+			sendEmail : function(formId){
+				_sendEmail(formId);
 			}
 		};
 		
@@ -40,20 +99,22 @@
 			 * 
 			 * @returns {Object} The singleton instance
 			 */
-			getInstance: function () {
-				if(!_config.instance ){
-					return null;
-				}
-				return _config.instance;
-			},			
-		
-			initializeInstance: function (initOptions) {
-				
-				if(!_config.instance && initOptions ){
-					_config.instance =  _createInstance(initOptions);
-				}
-				return this;				
-			}
-		};
-	})();
-}());
+			 getInstance: function () {
+			 	if(!_config.instance ){
+			 		return null;
+			 	}
+			 	return _config.instance;
+			 },			
+
+			 initializeInstance: function (initOptions) {
+
+			 	if(!_config.instance && initOptions ){
+			 		_config.instance =  _createInstance(initOptions);
+			 	}
+			 	return this;				
+			 }
+
+
+			};
+		})();
+	}());
